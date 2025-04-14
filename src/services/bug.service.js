072d160df49975b6cea304621c7af08bfd752c6c
daniel <daniel.evanenko@gmail.com>
2025-04-14 17:47:50 +0300

    init repo

---
+++ src/services/bug.service.js
@@ -0,0 +1,52 @@
+
+import Axios from 'axios'
+const axios = Axios.create({
+    withCredentials: true,
+})
+const BASE_URL = 'http://127.0.0.1:3030/api/bug/'
+
+export const bugService = {
+    query,
+    getById,
+    save,
+    remove,
+}
+
+
+async function query() {
+    try {
+        const { data: bugs } = await axios.get(BASE_URL)
+        return bugs
+    } catch (err) {
+        console.log('err:', err)
+        throw err
+    }
+}
+async function getById(bugId) {
+    try {
+        const { data: bug } = await axios.get(BASE_URL + bugId)
+        return bug
+    } catch (err) {
+        console.log('err:', err)
+        throw err
+    }
+
+}
+async function remove(bugId) {
+    try {
+        const { data: bug } = await axios.get(BASE_URL + bugId + '/remove')
+        return bug
+    } catch (err) {
+        console.log('err:', err)
+        throw err
+    }
+}
+async function save(bug) {
+    try {
+        const { data: savedBug } = await axios.get(BASE_URL + 'save', { params: bug })
+        return savedBug
+    } catch (err) {
+        console.log('err:', err)
+        throw err
+    }
+}
